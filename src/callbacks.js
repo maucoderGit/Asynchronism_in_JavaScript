6b1885547900a45ae1f4017a3b6629216369cd9f
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-27 22:33:33 -0400

    v 0.1

--- src/callbacks.js
+++ src/callbacks.js
@@ -7,3 +7,17 @@ function calc(first_number, second_number, callback){
 };
 
 console.log(calc(2,2, sum));
+
+function date(callback){
+    console.log(new Date);
+    setTimeout(function (a,b) {
+        let date = new Date;
+        callback(date);
+    }, 3000)
+};
+
+function printDate(dateNow){
+    console.log(dateNow);
+};
+
+date(printDate);
