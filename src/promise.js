b6a36f5b09e87f3b475afeaecbb1eb4bb3935390
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-29 20:46:17 -0400

    Understanding promises and Asynchronism in JS

--- src/promise.js
+++ src/promise.js
@@ -11,3 +11,28 @@ const something_will_happen = () => {
 something_will_happen()
     .then(response => console.log(response)) 
     .catch(err => console.log(err));
+
+const something_will_happen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true){
+            setTimeout (() =>{
+                resolve('True')
+            }, 2000)
+        } else {
+            const error = new Error('Whoops!')
+            reject(error)
+        }
+    })
+}
+
+something_will_happen2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+Promise.all([something_will_happen(), something_will_happen2()])
+    .then(response => {
+        console.log(`Array of results ${response}`)
+    })
+    .catch(err => {
+        console.error(err)
+    })
