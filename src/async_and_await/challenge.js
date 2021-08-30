e6da269bda3c863bc7b60b8644e63e530104ac8b
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-30 12:39:24 -0400

    getting data of rickandmortyapi
    adding try and catch.
    testing promises and async and await

--- src/async_and_await/challenge.js
+++ src/async_and_await/challenge.js
@@ -1,10 +1,20 @@
 const fetch_data = require("--/utils/fetch_data")
 const API = "https://rickandmortyapi.com/api/character/"
 
-const another_function = async (url_apu) => {
+const another_function = async (url_api) => {
     try{
         const data = await fetch_data(url_api)
+        const character = await fetch_data(`${API}${data.results[0].id}`)
+        const origin = await fetch_data(characcter.origin.url)
+    
+        console.log(data.info.count)
+        console.log(character.name)
+        console.log(origin.dimension)
     } catch (error) {
         console.error(error)
     }
 }
+
+console.log("Before")
+another_function(API)
+console.log("After")
