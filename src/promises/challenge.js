db2f600849ee990272a670c6493a707efa8bbacd
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-29 22:16:52 -0400

    Testing challenge

--- src/promises/challenge.js
+++ src/promises/challenge.js
@@ -1,1 +1,16 @@
-const fetch_data = require('...')
+const fetch_data = require('../utils/fetch_data')
+const API = 'https://rickandmortyapi.com/api/character/'
+
+fetch_data(API)
+    .then (data => {
+        console.log(data.info.count)
+        return fetch_data(`${API}${data.result[0].id}`)
+    })
+    .then (data => {
+        console.log(data.name)
+        return fetch_data(data.origin.url)
+    })
+    .then (data => {
+        console.log(data.dimension)
+    })
+    .catch(err => console.error(err))
