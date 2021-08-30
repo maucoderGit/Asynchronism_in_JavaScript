f13331b7cc1e5873a315ed0c7f11b97455802477
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-30 12:32:46 -0400

    try and catch added

--- src/async_and_await/challenge.js
+++ src/async_and_await/challenge.js
@@ -1,6 +1,10 @@
 const fetch_data = require("--/utils/fetch_data")
 const API = "https://rickandmortyapi.com/api/character/"
 
-const another_function = async () => {
-    
+const another_function = async (url_apu) => {
+    try{
+        const data = await fetch_data(url_api)
+    } catch (error) {
+        console.error(error)
+    }
 }
